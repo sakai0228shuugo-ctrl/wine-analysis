2a48070b1e4ec77caaf369cea10237f94812dd20
ShugoSakai <sakai0228shuugo@gmail.com>
2026-07-15 13:35:31 +0900

    update:main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-import numpy as np
 import pandas as pd
-import matplotlib
-print(np.__version__)
-print(pd.__version__)
-print(matplotlib.__version__)
+
+df = pd.read_csv('data/wine.csv')
+
+print(df.info())
+print(df.describe())
+print(df.head())
